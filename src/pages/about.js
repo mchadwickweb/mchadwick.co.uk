98db16be57e927d86f5afcdb4247a59ab6c6f606
Michael Chadwick <michael.chadwick@weareaccess.co.uk>
2018-10-19 18:14:24 +0100

    Dev: Updated some styles on headings and refactored the page structure markup.

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,13 @@ import Layout from '../components/layout';
 
 const About = () => (
   <Layout>
-    <h1>About</h1>
+    <header className="content__header">
+      <h1>A northern lad that loves building stuff and getting lost up mountains.</h1>
+      <p></p>
+    </header>
+    <article className="content__article">
+
+    </article>
   </Layout>
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,14 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <div className="intro">
+    <header className="content__header">
       <h1>Hey, my name is Michael Chadwick. I am a senior front-end developer based in Manchester.</h1>
+    </header>
+    <article className="content__article">
       <p>I have spent the past 6+ years working with clients such as National Grid, Autovista Group, WaterAid and Co-op Insurance to build awesome and accessible front-ends to large scale Drupal applications and websites. I love tech and seeing it impact people for the better.</p>
       <Link className="button" to="/contact/">Contact me</Link>
-    </div>
-  </Layout>
+    </article>
+  </Layout >
 )
 
 export default IndexPage
