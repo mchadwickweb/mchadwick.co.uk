d540173414716b958c7e418e39a2e2fb2c4e6b4d
Michael Chadwick <michael.chadwick@weareaccess.co.uk>
2018-10-25 14:51:00 +0100

    Adding sitemap.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: 'Michael Chadwick - Front end Developer based in Manchester, UK',
+    siteUrl: 'https://www.mchadwick.co.uk',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-offline',
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        output: '/sitemap.xml',
+        exclude: ['/success'],
+      }
+    }
   ],
 }
